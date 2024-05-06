052ab0cef243f6916c5d12edc918f2920db7dcf8
left <107749872+0xleft@users.noreply.github.com>
2024-05-06 20:24:58 +0100

    Fuzzing finishing

--- task.sh
+++ task.sh
@@ -1,14 +1,28 @@
 set -e
 
 if [ "$1" == "test" ]; then
-    echo "Running tests..."
-    sudo docker compose -f "test.docker-compose.yml" up --build
-    exit 0
+    if [ "$2" == "d" ] ; then # down
+        echo "Stopping tests..."
+        sudo docker compose -f "test.docker-compose.yml" down
+        exit 0
+    fi
+    if [ "$2" == "" ]; then # up
+        echo "Running tests..."
+        sudo docker compose -f "test.docker-compose.yml" up -d --build
+        exit 0
+    fi
 fi
 if [ "$1" == "fuzz" ]; then
-    echo "Running fuzzing..."
-    sudo docker compose -f "fuzzing.docker-compose.yml" up --build
-    exit 0
+    if [ "$2" == "d" ]; then
+        echo "Stopping fuzzing..."
+        sudo docker compose -f "fuzzing.docker-compose.yml" down
+        exit 0
+    fi
+    if [ "$2" == "" ]; then # up
+        echo "Running fuzzing..."
+        sudo docker compose -f "fuzzing.docker-compose.yml" up -d --build
+        exit 0
+    fi
 fi
 
-echo "Usage: ./task.sh test|fuzz" 
+echo "Usage: ./task.sh test|fuzz [d]" # d for down
