1bafdbbe44d787edf0fc8fc6875ac4e0ab769dec
left <107749872+0xleft@users.noreply.github.com>
2024-05-07 22:06:10 +0100

    Update fuzz.sh

--- fuzzing/fuzz.sh
+++ fuzzing/fuzz.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/aflnet/afl-fuzz -i /app/fuzzing/seed -o out-asmhost -N tcp://127.0.0.1/9001 -P HTTP -m none -d -q 3 -s 3 -E -K -R -Q -D 20 -m none main
+/opt/aflnet/afl-fuzz -i /app/fuzzing/seed -o out-asmhost -N tcp://127.0.0.1/9001 -P HTTP -m none -d -q 3 -s 3 -K -R -Q -D 20 main
